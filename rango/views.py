d06c1b49c5cd147ba1f35f1bc0f54446134f123d
Jonathan Ng <jonathantyng@gmail.com>
2015-09-05 19:08:01 -0400

    Added cookie functionality

--- rango/views.py
+++ rango/views.py
@@ -5,13 +5,34 @@ from rango.forms import CategoryForm,PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect, HttpResponse
+from datetime import datetime
 
 def index(request):
   category_list = Category.objects.order_by('-likes')[:5]
   context_dict = {'categories': category_list}
   pages_list = Page.objects.order_by('-views')[:5]
   context_dict['pages'] = pages_list
-  return render(request, 'rango/index.html', context_dict)
+
+  visits = request.session.get('visits')
+  if not visits:
+    visits = 1
+  reset_last_visit_time = False
+  last_visit = request.session.get('last_visit')
+  if last_visit:
+    last_visit_time = datetime.strptime(last_visit[:-7],"%Y-%m-%d %H:%M:%S")
+
+    if (datetime.now() - last_visit_time).days > 0:
+      visits = visits + 1
+      reset_last_visit_time = True
+  else:
+    reset_last_visit_time = True
+
+  if reset_last_visit_time:
+    request.session['last_visit'] = str(datetime.now())
+    request.session['visits'] = visits
+
+  context_dict['visits']=visits
+  return render(request,'rango/index.html',context_dict)
 
 def about(request):
   context_dict = {'boldmessage': "ABOUT ABOUT ABOUT!!!"}
