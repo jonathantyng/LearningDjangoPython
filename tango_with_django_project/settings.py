39064cafbb5d8d3c6f987126aa3af0d86338a989
Jonathan Ng <jonathantyng@gmail.com>
2015-09-06 16:56:14 -0400

    Updated registration functionality

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -38,7 +38,14 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 )
+
+REGISTRATION_OPEN = True
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = '/rango/'
+LOGIN_URL = '/accounts/login'
 
 TEMPLATE_DIRS = (TEMPLATE_PATH,)
 
